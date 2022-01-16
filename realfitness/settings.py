fbbb508527afdb81da3d2824c5277fa89d62b048
patrickf949 <43245704+patrickf949@users.noreply.github.com>
2022-01-16 18:21:28 +0300

    fix issues preventing app from starting
    - renaming faulty apps in settings and wsgi apps

--- realfitness/settings.py
+++ realfitness/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'ReaLfitness.urls'
+ROOT_URLCONF = 'realfitness.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'ReaLfitness.wsgi.application'
+WSGI_APPLICATION = 'realfitness.wsgi.application'
 
 
 # Database
